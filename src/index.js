4ae3c455a7ea153a56c991919fe278889a63d344
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-10 11:11:31 -0500

    connected application through redux with thunk and logger middleware packages attached

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
+
+//1. Add in all necessary components and libary methods.
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers/index';
+
+//2. Create a store that includes thunk and logger middleware support.
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
-const rootElement = document.getElementById("root");
-
+const rootElement = document.getElementById('root');
+//3. Wrap the App component in a react-redux Provider element.
 ReactDOM.render(
-    <App />, 
-    rootElement
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
 );
 
 //Task List:
