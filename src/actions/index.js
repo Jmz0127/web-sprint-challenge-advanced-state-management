c1e929058d6221556a179e259a0467988911260f
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-10 19:34:56 -0500

    added two actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,6 +18,14 @@ export const fetchSmurfs = () => (dispatch) => {
 			dispatch({ type: ERROR, payload: err.message });
 		});
 };
+
+export const addSmurf = () => {
+	return { type: ADDING_SMURF };
+};
+
+export const smurfError = () => {
+	return { type: ADDING_ERROR };
+};
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
